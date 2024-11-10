dfe2ed74b88175cf4c6152bf3217f4829356d8ff
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 20:02:29 +0530

    Rename notifications.py to notification.py and add methods for retrieving and dismissing notifications

--- swiftmail/api/models/notification.py
+++ swiftmail/api/models/notification.py
@@ -1,3 +1,4 @@
+import time
 from enum import Enum
 
 from pydantic import BaseModel, Field
@@ -27,5 +28,17 @@ class Notification(BaseModel):
     date_delivered: int | None = Field(None, alias="date_delivered")
     date_failed: int | None = Field(None, alias="date_failed")
 
+    @staticmethod
+    def get_by_id(id: str) -> "Notification | None":
+        doc = NOTIFICATIONS_COLLECTION.document(id).get()
+        if doc.exists:
+            return Notification(**doc.to_dict())  # type:ignore
+        return None
+
     def create(self):
+        NOTIFICATIONS_COLLECTION.document(self.id).set(self.model_dump())
+
+    def dismiss(self):
+        self.status = NotificationStatus.DISMISSED
+        self.date_updated = int(time.time())
         NOTIFICATIONS_COLLECTION.document(self.id).set(self.model_dump())
