ec8cb60c066b54b673f5abea257c9c440fff270e
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 20:02:38 +0530

    Add dismiss endpoint for notifications with request validation and ownership check

--- swiftmail/api/routes/notification/__init__.py
+++ swiftmail/api/routes/notification/__init__.py
@@ -1,11 +1,16 @@
 from flask import Blueprint
 
 from swiftmail.api.middlewares.auth import firebase_auth_middleware
+from swiftmail.api.routes.notification.dismiss import dismiss
 
 notification_router = Blueprint("notification_router", __name__)
 
 # Middlewares
 notification_router.before_request(firebase_auth_middleware)
+
+
+# Routes
+notification_router.add_url_rule("/dismiss", view_func=dismiss, methods=["PATCH"])
 
 
 @notification_router.route("/")
