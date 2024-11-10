eddfae9ca4ccb96ec2b94e5d979826c6973f272e
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 19:49:37 +0530

    Add create endpoint for digest with request validation

--- swiftmail/api/routes/digest/__init__.py
+++ swiftmail/api/routes/digest/__init__.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 
 from swiftmail.api.middlewares.auth import firebase_auth_middleware
 
+from .create import create
+
 digest_router = Blueprint("digest_router", __name__)
 
 # Middlewares
@@ -11,3 +13,6 @@ digest_router.before_request(firebase_auth_middleware)
 @digest_router.route("/")
 def index():
     return "OK", 200
+
+
+digest_router.add_url_rule("/create", view_func=create, methods=["POST"])
