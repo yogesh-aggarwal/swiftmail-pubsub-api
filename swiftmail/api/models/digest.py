5df453d05ed8ef9c4a5db907bf64ee26bff4041a
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 19:56:44 +0530

    Add methods to retrieve and update Digest model attributes

--- swiftmail/api/models/digest.py
+++ swiftmail/api/models/digest.py
@@ -1,3 +1,4 @@
+import time
 from pydantic import BaseModel, Field
 
 from swiftmail.core.firebase import DIGESTS_COLLECTION
@@ -12,5 +13,22 @@ class Digest(BaseModel):
     title: str = Field(..., alias="title")
     description: str = Field(..., alias="description")
 
+    @staticmethod
+    def get_by_id(id: str) -> "Digest | None":
+        doc = DIGESTS_COLLECTION.document(id).get()
+        if doc.exists:
+            return Digest(**doc.to_dict())  # type:ignore
+        return None
+
     def create(self):
+        DIGESTS_COLLECTION.document(self.id).set(self.model_dump())
+
+    def update_title(self, title: str):
+        self.title = title
+        self.date_updated = int(time.time())
+        DIGESTS_COLLECTION.document(self.id).set(self.model_dump())
+
+    def update_description(self, description: str):
+        self.description = description
+        self.date_updated = int(time.time())
         DIGESTS_COLLECTION.document(self.id).set(self.model_dump())
