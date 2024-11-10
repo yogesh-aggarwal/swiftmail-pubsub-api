0adad10dc0a18805846a30b1dd0af1bc3ca1449f
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 20:06:45 +0530

    Add status endpoint for notifications with validation and ownership checks

--- swiftmail/api/routes/notification/__init__.py
+++ swiftmail/api/routes/notification/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 
 from swiftmail.api.middlewares.auth import firebase_auth_middleware
-from swiftmail.api.routes.notification.dismiss import dismiss
+
+from .dismiss import dismiss
+from .status import status
 
 notification_router = Blueprint("notification_router", __name__)
 
@@ -11,6 +13,7 @@ notification_router.before_request(firebase_auth_middleware)
 
 # Routes
 notification_router.add_url_rule("/dismiss", view_func=dismiss, methods=["PATCH"])
+notification_router.add_url_rule("/status", view_func=status, methods=["PATCH"])
 
 
 @notification_router.route("/")
