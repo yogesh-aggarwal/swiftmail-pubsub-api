3150121ea3eb588e415b3045cca5132a1e9cd11c
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-10 19:58:15 +0530

    Add delete endpoint for digest with request validation and ownership check

--- swiftmail/api/routes/digest/__init__.py
+++ swiftmail/api/routes/digest/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from swiftmail.api.middlewares.auth import firebase_auth_middleware
 
 from .create import create
+from .delete import delete
 from .update import update
 
 digest_router = Blueprint("digest_router", __name__)
@@ -17,4 +18,5 @@ def index():
 
 
 digest_router.add_url_rule("/create", view_func=create, methods=["POST"])
+digest_router.add_url_rule("/delete", view_func=delete, methods=["DELETE"])
 digest_router.add_url_rule("/update", view_func=update, methods=["PATCH"])
