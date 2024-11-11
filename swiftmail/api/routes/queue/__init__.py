1a3efe156fc8e51379b2a7dff926516c56cfa301
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-11 22:16:13 +0530

    Add Firebase authentication middleware and refactor new message processing

--- swiftmail/api/routes/queue/__init__.py
+++ swiftmail/api/routes/queue/__init__.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
 
 from .new_message import new_message
+from swiftmail.api.middlewares.auth import firebase_auth_middleware
 
 queue_router = Blueprint("queue_router", __name__)
+
+# Middlewares
+queue_router.before_request(firebase_auth_middleware)
 
 queue_router.add_url_rule("/new-message", view_func=new_message, methods=["POST"])
--- swiftmail/api/routes/queue/new_message.py
+++ swiftmail/api/routes/queue/new_message.py
@@ -1,47 +1,26 @@
-from rich import print
-
 from flask import request
 
-from swiftmail.api.models.message import Message
-from swiftmail.api.models.user import User, UserPreferences
-from swiftmail.factory.prompts import PromptFactory
-from swiftmail.services.llm.utils import get_llm_from_string
-
-
-def _process_message(
-    subject: str,
-    html_content: str,
-    user_prefs: UserPreferences,
-) -> Message | None:
-    llm_model = get_llm_from_string(user_prefs.ai.model)
-    llm_prompt = PromptFactory.email_segregation(
-        email_subject=subject,
-        email_html_content=html_content,
-        user_bio=user_prefs.ai.self_description,
-        user_defined_priorities=user_prefs.inbox.priorities,
-        user_defined_priority_rules=user_prefs.inbox.priority_rules,
-        user_defined_labels=user_prefs.inbox.labels,
-        user_defined_label_rules=user_prefs.inbox.label_rules,
-        user_defined_categories=user_prefs.inbox.categories,
-        user_defined_category_rules=user_prefs.inbox.category_rules,
-        user_defined_spam_words=user_prefs.inbox.spam_words,
-        user_defined_spam_rules=user_prefs.inbox.spam_rules,
-        user_defined_unsubscribe_words=user_prefs.inbox.unsubscribe_words,
-        user_defined_unsubscribe_rules=user_prefs.inbox.unsubscribe_rules,
-        enforce_not_spam=False,
-    )
-
-    print(llm_model, llm_prompt)
+from swiftmail.api.models.message import MessageEmailData
+from swiftmail.jobs.process_email import ProcessEmailJobData, process_email
 
 
 def new_message():
     user: User = getattr(request, "user", None)  # type:ignore
-    print(user)
 
-    _process_message(
-        subject="Test",
-        html_content="Test",
-        user_prefs=user.data.preferences,
+    process_email(
+        ProcessEmailJobData(
+            email=MessageEmailData(
+                subject="Sample Subject",
+                html_content="<p>This is a test email content.</p>",
+                message_id="sample_message_id",
+                thread_id="sample_thread_id",
+                from_email="from@example.com",
+                to_email="to@example.com",
+                cc_email="cc@example.com",
+                bcc_email="bcc@example.com",
+            ),
+            user=user,
+        ).model_dump_json()
     )
 
     return "OK", 200
