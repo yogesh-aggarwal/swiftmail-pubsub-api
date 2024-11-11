a77a3954ff02c5ca4fb1302cb1155c7d76e45bf8
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-11 20:06:03 +0530

    Update get_llm_from_string function to specify return type as LLMService

--- swiftmail/services/llm/utils.py
+++ swiftmail/services/llm/utils.py
@@ -1,8 +1,9 @@
+from . import LLMService
 from .gpt4omini import GPT4oMini
 from .mistral_7b import Mistral7B
 
 
-def get_llm_from_string(name: str):
+def get_llm_from_string(name: str) -> LLMService:
     match name:
         case "gpt4omini":
             return GPT4oMini()
