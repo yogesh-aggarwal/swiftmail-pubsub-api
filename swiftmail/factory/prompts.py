106cc34aa42081c9572eb6a75b11d14ab1ff8e04
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-11 14:58:47 +0530

    Add email digest segregation functionality with JSON input format

--- swiftmail/factory/prompts.py
+++ swiftmail/factory/prompts.py
@@ -23,6 +23,57 @@ class PromptFactory:
         )
 
     @staticmethod
+    def email_digest_segregate(
+        *,
+        # Digests
+        user_digest_ids: list[str],
+        user_digest_titles: list[str],
+        user_digest_descriptions: list[str],
+        # Emails
+        email_id: str,
+        email_subject: str,
+        email_content: str,
+    ):
+        if (
+            len(user_digest_ids) != len(user_digest_titles)
+            or len(user_digest_ids) != len(user_digest_descriptions)
+            or len(user_digest_titles) != len(user_digest_descriptions)
+        ):
+            raise ValueError("All lists must be of the same length")
+
+        with open("assets/email_digest_segregate/system.txt") as f:
+            system_prompt = f.read()
+        with open("assets/email_digest_segregate/user.txt") as f:
+            user_prompt = f.read().strip()
+            user_prompt = user_prompt.format(
+                input_json=json.dumps(
+                    {
+                        "digests": [
+                            {
+                                "id": user_digest_ids[i],
+                                "title": user_digest_titles[i],
+                                "description": user_digest_descriptions[i],
+                            }
+                            for i in range(len(user_digest_ids))
+                        ],
+                        "emails": {
+                            "id": email_id,
+                            "subject": email_subject,
+                            "html_content": email_content,
+                        },
+                    }
+                ),
+            )
+
+        return LLMPrompt(
+            "Email Digest Segregate",
+            [
+                LLMMessage("system", system_prompt),
+                LLMMessage("user", user_prompt),
+            ],
+        )
+
+    @staticmethod
     def email_segregation(
         *,
         # User about
