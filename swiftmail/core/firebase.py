53707ebaa2440fefba4a87c8785c12d8c6b4ee5c
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-18 17:32:49 +0530

    Added back firebaze.py for auth

--- swiftmail/core/firebase.py
+++ swiftmail/core/firebase.py
@@ -1,19 +1,9 @@
 import firebase_admin
-from firebase_admin import auth, credentials, firestore
+from firebase_admin import auth, credentials
 
 from .constants import GCP_CREDENTIALS_FILE
 
 cred = credentials.Certificate(GCP_CREDENTIALS_FILE)
 firebase_admin.initialize_app(cred)
 
-db = firestore.client()
 auth = auth
-
-DASHBOARDS_COLLECTION = db.collection("dashboards")
-DATA_COLLECTION = db.collection("data")
-DIGESTS_COLLECTION = db.collection("digests")
-MESSAGES_COLLECTION = db.collection("messages")
-NOTIFICATIONS_COLLECTION = db.collection("notifications")
-REMINDERS_COLLECTION = db.collection("reminders")
-THREADS_COLLECTION = db.collection("threads")
-USERS_COLLECTION = db.collection("users")
