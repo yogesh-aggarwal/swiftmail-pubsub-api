124249f4854992ddd2aa07c9ef3d38abea552761
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-11-28 04:47:18 +0530

    Disabled authentication in queue for now

--- swiftmail/api/rest/routes/queue/__init__.py
+++ swiftmail/api/rest/routes/queue/__init__.py
@@ -7,7 +7,7 @@ from swiftmail.api.middlewares.auth import firebase_auth_middleware
 queue_router = Blueprint("queue_router", __name__)
 
 # Middlewares
-queue_router.before_request(firebase_auth_middleware)
+# queue_router.before_request(firebase_auth_middleware)
 
 queue_router.add_url_rule("/new-message", view_func=new_message, methods=["POST"])
 queue_router.add_url_rule("/import", view_func=import_messages, methods=["POST"])
